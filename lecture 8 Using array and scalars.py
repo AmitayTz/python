751f6bed391b3770553eca1eaadd8c1113cbd562
amitay123456 <86182853+amitay123456@users.noreply.github.com>
2022-01-28 16:16:08 +0200

    Add files via upload

---
+++ lecture 8 Using array and scalars.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+
+
+# In[2]:
+
+
+from __future__ import division # import division
+
+
+# In[3]:
+
+
+5/2
+
+
+# In[7]:
+
+
+arr1 = np.array([[1,2,3,4],[8,9,10,11]])
+
+arr1
+# In[8]:
+
+
+arr1*arr1   #like 1*1,2*2,3*3.....
+
+
+# In[9]:
+
+
+arr1 - arr1 #like 1-1,2-2,3-3...
+
+
+# In[10]:
+
+
+1/arr1      #1/1,2/1,3/1
+
+
+# In[11]:
+
+
+arr1 ** 3 # 1*1*1,2*2*2,3*3*3
+
+
+# In[ ]:
+
+
+
+
